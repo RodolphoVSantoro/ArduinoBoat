4099d18d11891e44662381753b7f5001d64e608a
Rodolpho Vianna Santoro <44331003+RodolphoVSantoro@users.noreply.github.com>
2022-07-31 15:36:47 -0300

    Add files via upload

---
+++ Motor/Motor.ino
@@ -0,0 +1,22 @@
+int velocidade = 5; //m1
+int direcao = 7;
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(direcao, OUTPUT);
+  pinMode(velocidade, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(direcao, LOW);
+  analogWrite(velocidade, 200);
+  delay(5000);
+  digitalWrite(velocidade, LOW);
+  delay(1000);
+  digitalWrite(direcao, HIGH);
+  analogWrite(velocidade, 200);
+  delay(5000);
+  digitalWrite(velocidade, LOW);
+  delay(1000);
+}
---
+++ servo/servo.ino
@@ -0,0 +1,56 @@
+#include <Servo.h> 
+ 
+Servo myservo;  
+char buffer[18];
+int valor;
+void setup() 
+{ 
+  Serial.begin(9600);
+  Serial.flush();
+  myservo.attach(9);
+} 
+
+int converte(char *data){
+  Serial.print("Data:");
+  Serial.println(data);
+  int val = strtol(data, NULL, 10);
+  
+  for(int x = 0; x<16; x++){
+    buffer[x] = '\0';
+  }
+  Serial.flush();
+  Serial.println(val);
+  return val;
+}
+
+void loop() 
+{
+  if(Serial.available() > 0){
+    int index=0;
+    delay(100);
+    int numChar = Serial.available();
+    if (numChar > 15){
+      numChar=15;
+    }
+    while(numChar--){
+      buffer[index++] = Serial.read();
+    }
+    valor = converte(buffer);
+  }
+  
+  switch(valor)
+  {
+  case 1:
+  Serial.println("foi");
+  myservo.write(135);
+  break;
+  
+  case 3:
+  myservo.write(45);
+  break;
+  
+  case 0:
+  myservo.write(90);
+  break;
+} 
+}
